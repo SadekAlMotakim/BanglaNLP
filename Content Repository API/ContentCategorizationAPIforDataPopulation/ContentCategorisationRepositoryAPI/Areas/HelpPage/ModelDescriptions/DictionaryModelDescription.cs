9507fbdcc3793aec4ddf9030fd2b8656128b9883
sadek al mostakim <s.a.mostakim@gmail.com>
2018-08-14 01:47:14 +0600

    1254

---
+++ Content Repository API/ContentCategorizationAPIforDataPopulation/ContentCategorisationRepositoryAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ContentCategorisationRepositoryAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Content Repository API/ContentCategorizationAPIforDataPopulation/ContentCategorisationRepositoryAPI/Models/Articles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContentCategorisationRepositoryAPI.Models
+{
+    public class Articles
+    {
+        public string article { get; set; }
+        public string category { get; set; }
+    }
+}
